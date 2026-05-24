6c1bec662d5dc465bb227123f223162bd86d3805
Yerkin Suimenbay <yerkin.suimenbay@gmail.com>
2026-05-24 21:20:04 +0500

    initial commit

---
+++ catalog-service/src/test/java/com/kinnn/catalogservice/TestCatalogServiceApplication.java
@@ -0,0 +1,11 @@
+package com.kinnn.catalogservice;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCatalogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.from(CatalogServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+    }
+
+}
